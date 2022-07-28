aca18fae0f0a9b8226a97de5ef9bb4a1551d5a70
Mursid S <97183746+Botwa021@users.noreply.github.com>
2022-07-28 22:43:07 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,16 +1,18 @@
-const qrku = "https://telegra.ph/file/95421a93a0ba051a21252.jpg"
+const qrmursid = "https://telegra.ph/file/5b967438c7a3337d349fa.jpg"
 
-let handler = async (m, { conn, usedPrefix }) => conn.sendButton(m.chat, `
-┏──「 Donasi • Dana 」─⬣
+let handler = async (m, { conn, usedPrefix }) => conn.sendButtonImg(m.chat, qrmursid, `
+┏──「 Donasi • E-money 」─⬣
 │ • Gopay : 088233832771
 │ • Dana : 088233832771
 │ • Smartfren : 088233832771
-╰────────⬣
+│ • Tri : 089674452300
+┗────────⬣
 ┏──「 *NOTE* 」─⬣
 │ > Ingin donasi? wa.me/6288233832771
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
-│ _24jam tanpa kendala_
+│ _24jam tanpa kendala, siapapun yang donasi_
+│ _akan saya kasih user premium selama 2 minggu_
 ┗────────⬣
 `.trim(), wm, 'Menu', usedPrefix + 'menu', m) // Tambah sendiri kalo mau
 handler.help = ['donasi']
